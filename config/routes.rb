51b9d4ea6cb54a568454f8b2a37128b561a89eeb
kouzouiiimaizumi <imaizumi@nacalai.co.jp>
2018-12-04 00:55:30 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'users/index'
 
-  resources :tasks
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'toppages/index'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  root to: 'toppages#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+  
+  resources :tasks, only: [:index, :show, :new, :create]
 end
